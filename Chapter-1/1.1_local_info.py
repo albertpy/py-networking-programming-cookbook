81e73d2b7ba8c01afa61d479e77ed940e605ff5e
albertpy <31097175+albertpy@users.noreply.github.com>
2017-08-17 17:30:45 +0700

    Create 1.1_local_info.py

---
+++ Chapter-1/1.1_local_info.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#Python Nework Programming Cookbook -- Chapter-1
+# This program is optimized for Python 2.7. It may run on
+# any Python version with/without modifications.
+
+import socket
+
+def print_machine_info():
+   host_name = socket.gethostname()
+   ip_address = socket.gethostbyname(host_name)
+   print "Host name: %s" % host_name
+   print "Ip address: %s" % ip_address
+
+if __name__ == '__main__':
+   print_machine_info()
